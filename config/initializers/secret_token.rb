0ed55030026b62b72823199e39541f7069959d8d
Balint Domokos <bdomokos74@yahoo.com>
2011-01-12 01:55:55 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoryTracker::Application.config.secret_token = 'a1d600e837e334ad0743ef44e1cbb7bcfd2c486f9a6ed59a89460fa9434a40a21f612c3a73b68175d6b4eb58018be03b2f82a7c137fa86a8f665aec3d4091ec8'
